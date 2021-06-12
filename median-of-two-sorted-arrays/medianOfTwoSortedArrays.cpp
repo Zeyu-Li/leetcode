8439eb46deadb0ede03c4c580cb9903e118377a1
Zeyu-Li <zeyu.li23@gmail.com>
2021-06-12 11:50:10 -0600

    fixed median

--- median-of-two-sorted-arrays/medianOfTwoSortedArrays.cpp
+++ median-of-two-sorted-arrays/medianOfTwoSortedArrays.cpp
@@ -1,43 +1,40 @@
-// is not optimal solution
 class Solution {
 public:
     double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
-        bool flag = true;
-        if (nums1.size()>nums2.size()) {
-            // move nums2 to nums1
-            for (auto it : nums2) {
-                nums1.push_back(it);
-                sort(nums1.begin(), nums1.end());
+        int finalSize = nums1.size() + nums2.size();
+        bool even = finalSize % 2 == 0 ? true : false;
+        
+        int ptr1, ptr2, lastptr, mid, previous;
+        ptr1 = ptr2 = mid = previous = 0;
+        // floor division
+        lastptr = finalSize / 2 + 1;
+        // trival case
+        if (nums1.size() == 0 && !even) { return nums2[lastptr-1]; }
+        else if (nums2.size() == 0 && !even) { return nums1[lastptr-1]; }
+        else if (nums1.size() == 0) { return ((double) nums2[lastptr-2] + nums2[lastptr-1]) / 2; }
+        else if (nums2.size() == 0) { return ((double) nums1[lastptr-2] + nums1[lastptr-1]) / 2; }
+
+        for (int i = 0; i < lastptr; i++) {
+            previous = mid;
+            if (ptr1 == nums1.size()) {
+                mid = nums2[ptr2];
+                ptr2++;
+                continue;
             }
-        } else {
-            flag = false;
-            // move nums1 to nums2
-            for (auto it : nums1) {
-                nums2.push_back(it);
-                sort(nums2.begin(), nums2.end());
+            
+            if (ptr2 == nums2.size() || nums1[ptr1] < nums2[ptr2]) {
+                mid = nums1[ptr1];
+                ptr1++;
+            } else {
+                mid = nums2[ptr2];
+                ptr2++;
             }
         }
-        
-        if (flag) {
-            int size = nums1.size();
-            if (size%2) {
-                // floor divides then returns middle casting to double
-                return (double) nums1[size/2];
-            } else {
-                // average middle
-                int first = size/2;
-                return (double) (nums1[first]+nums1[first-1])/2.0;
-            }
+        if (!even) {
+            // cast to double
+            return (double) mid;
         } else {
-            int size = nums2.size();
-            if (size%2) {
-                // floor divides then returns middle casting to double
-                return (double) nums2[size/2];
-            } else {
-                // average middle
-                int first = size/2;
-                return (double) (nums2[first]+nums2[first-1])/2.0;
-            }
+            return ((double) previous + mid) / 2;
         }
     }
 };
