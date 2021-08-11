c500e9ab9c335abe27182d4aa1f6fc21f901dc41
Zeyu-Li <zeyu.li23@gmail.com>
2021-08-11 12:03:12 -0600

    add sum

---
+++ add-digits/add-digits.py
@@ -0,0 +1,6 @@
+class Solution:
+    def addDigits(self, num: int) -> int:
+        while (len(str(num)) != 1):
+            num = sum(int(char) for char in str(num))
+            
+        return num
